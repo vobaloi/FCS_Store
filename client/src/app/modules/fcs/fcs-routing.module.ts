3028a9f662055e5a9ff85e87364b5b37df40ae5b
vobaloi <vobaloi2k@gmail.com>
2022-05-14 08:02:06 +0700

    new

--- client/src/app/modules/fcs/fcs-routing.module.ts
+++ client/src/app/modules/fcs/fcs-routing.module.ts
@@ -5,7 +5,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', component: FcsContainComponent, children: [
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    
   ]}
 ];
 
